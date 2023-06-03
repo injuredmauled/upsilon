ca8a7716f02d76350cc8e5045ab8ebaedfa73f59
Almujeer Uddin <uddinalmujeer.27@gmail.com>
2023-06-03 14:24:50 +0900

    initial setup for tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
+<script setup lang="ts">
+import { Ref, ref } from 'vue'
+
+interface NavLink {
+  to: string
+  text: string
+}
+
+const links: Ref<NavLink[]> = ref([
+  { to: 'home', text: 'Home' },
+  { to: 'about', text: 'About Us' },
+])
+</script>
+
 <template>
   <header>
     <nav>
-      <ul>
-        <li>
-          <RouterLink :to="{ name: 'home' }">Home</RouterLink>
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'about' }">About</RouterLink>
-        </li>
+      <ul class="flex flex-row flex-nowrap gap-3 p-4">
+        <RouterLink v-for="{ to, text } in links" :to="{ name: to }" #="{ isActive }">
+          <li :class="['bg-lime-200 px-2 py-1 rounded-lg text-lime-800', { 'bg-lime-400 text-lime-950': isActive }]">
+            {{ text }}
+          </li>
+        </RouterLink>
       </ul>
     </nav>
   </header>
-  <RouterView />
+  <main class="p-4">
+    <RouterView />
+  </main>
 </template>
