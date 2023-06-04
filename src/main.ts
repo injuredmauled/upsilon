791ddaacc8677a61dcc432624c677c7f0c3a60e1
Almujeer Uddin <uddinalmujeer.27@gmail.com>
2023-06-04 13:13:11 +0900

    setup path alias

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
+import router from '@/router'
+import App from '@/App.vue'
 
-import './style.css'
+import '@/style.css'
 
 const app = createApp(App)
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,12 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'home',
-    component: () => import('../views/HomeView.vue'),
+    component: () => import('@/views/HomeView.vue'),
   },
   {
     path: '/about',
     name: 'about',
-    component: () => import('../views/AboutView.vue'),
+    component: () => import('@/views/AboutView.vue'),
   },
 ]
 
